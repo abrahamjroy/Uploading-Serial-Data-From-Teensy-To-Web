37b4a39ca7a80c18b642ce048a87f19301a735da
abrahamjroy <abrahamjroy@users.noreply.github.com>
2017-04-25 22:56:07 +0530

    Update browser_vis_serial.py

--- browser_vis_serial.py
+++ browser_vis_serial.py
@@ -1,17 +1,10 @@
+# Run this Python script on your terminal to get them dope readings
 import serial
 import sys
 import bottle
-
-
-
 @bottle.route('/arduino/')
 def getArduino():
-	#/dev/tty.<port where your arduino is>
-	#this will probably not be the same as mine
-	#you can find it by entering ls /dev/tty.* to see your 
-	#ports.  Also in Arduino IDE, go to Tools> Serial Port to 
-	#see which one you're at
-	ser = serial.Serial('/dev/ttyACM0',9600)
+	ser = serial.Serial('/dev/ttyACM0',9600)#Change this according to your device location
 	a = ser.readline()
 	d = {}
 	d['val']=a
@@ -20,8 +13,4 @@ def getArduino():
 @bottle.route('/')
 def index():
 	return open('index.html','r')
-	
-#These are for debugging, you can uncomment these, and comment out the bottle.run() if you need to debug
-#bottle.debug(True)
-#bottle.run(reloader = True )
 bottle.run()
