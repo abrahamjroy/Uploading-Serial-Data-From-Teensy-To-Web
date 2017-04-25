bd2858fb6f62a872eaf2cbf339a4a63dccd84b96
abrahamjroy <abrahamjroy@users.noreply.github.com>
2017-04-25 22:42:56 +0530

    Update TheArduinoCode.ino

--- TheArduinoCode.ino
+++ TheArduinoCode.ino
@@ -1,9 +1,9 @@
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+#include <Adafruit_Sensor.h>//YOU NEED THESE LIBRARIES
+#include <DHT.h>//YOU NEED THESE LIBRARIES
+#include <DHT_U.h>//YOU NEED THESE LIBRARIES
 
-#define DHTPIN            0      
-#define DHTTYPE           DHT11      
+#define DHTPIN            0 //Pin where the DHT11 Temprature and Humidity sensors are connected     
+#define DHTTYPE           DHT11 //Used to assign the type of DHT sensor used    
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
@@ -17,11 +17,14 @@ void loop() {
   delay(delayMS);
   sensors_event_t event;  
   dht.temperature().getEvent(&event);
+  Serial.print("Temprature:");
   Serial.print(event.temperature);
   Serial.print(",");
   dht.humidity().getEvent(&event);
+  Serial.print("Humidity:");
   Serial.print(event.relative_humidity);
   Serial.print(",");
+  Serial.print("CO2 PPM:");
   int sensorValue = analogRead(1);
   Serial.println(sensorValue);
 }
